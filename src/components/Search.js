936dd1c994d7525efea86254b60a8ee79c97a2af
Kesik93 <michal.kesik93@gmail.com>
2021-02-21 19:45:32 +0100

    instalacja fontawesome, dodano font w search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Search() {
     const handleSubmit = (event) => {
@@ -9,7 +11,8 @@ function Search() {
     return (
         <div className='search'>
             <form onSubmit={handleSubmit}>
-                <input className='search__input' type="text" placeholder="Search..." />
+                <FontAwesomeIcon icon={faSearch} className='search_icon'/>
+                <input className='search__input' type="text" placeholder="Type something..." />
                 <button className='search__button'>Search</button>
             </form>
         </div>
